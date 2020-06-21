be67dd82297ddae5653b591da12cab55f705845e
Jack LaFond <jack.lafond@icloud.com>
2020-06-21 00:18:23 -0400

    much better help command

--- bot.py
+++ bot.py
@@ -7,18 +7,28 @@ bot = commands.Bot(command_prefix=config.prefix)
 
 bot.config = config
 
+
 @bot.event
 async def on_ready():
+    bot.remove_command("help")
     for cog in config.cogs:
         bot.load_extension(cog)
-    
-    print("\nWelcome to Nanocord! I'm going to try to connect to your Nanoleaf Aurora panels that you've specified in your config.")
 
-    req = requests.get(f'http://{config.aurora_ip}:16021/api/v1/{config.aurora_auth}')
+    print(
+        "\nWelcome to Nanocord! I'm going to try to connect to your Nanoleaf Aurora panels that you've specified in your config."
+    )
+
+    req = requests.get(f"http://{config.aurora_ip}:16021/api/v1/{config.aurora_auth}")
     json = req.json()
-    if json['name']:
-        print(f"Nanoleaf Auroras found! I see {json['name']} with a firmware version of {json['firmwareVersion']} and a serial number of {json['serialNo']}.\nIf these panels are the incorrect ones, double check the IP you specified in the console. Elsewise, enjoy the bot!\n\nCreated by Jack LaFond (@jacc on GitHub)\n-- Special Thanks:\n- @venoras/Huecord\n\nAny issues? Feel free to open an issue on the GitHub repository. PRs are welcomed!")
+    if json["name"]:
+        print(
+            f"Nanoleaf Auroras found! I see {json['name']} with a firmware version of {json['firmwareVersion']} and a serial number of {json['serialNo']}.\nIf these panels are the incorrect ones, double check the IP you specified in the console. Elsewise, enjoy the bot!\n\nCreated by Jack LaFond (@jacc on GitHub)\n-- Special Thanks:\n- @venoras/Huecord\n\nAny issues? Feel free to open an issue on the GitHub repository. PRs are welcomed!"
+        )
     else:
-        print("I couldn't find any panels with that IP! Double check the config and try again.")
+        print(
+            "I couldn't find any panels with that IP! Double check the config and try again."
+        )
+
+
 if __name__ == "__main__":
     bot.run(config.token)
--- cogs/basic.py
+++ cogs/basic.py
@@ -6,6 +6,7 @@ from discord import Webhook, RequestsWebhookAdapter, File
 import json
 from discord.ext.commands import has_permissions, MissingPermissions
 
+
 class Essentials(commands.Cog, name="Essential Commands"):
     def __init__(self, bot):
         self.bot = bot
@@ -19,6 +20,27 @@ class Essentials(commands.Cog, name="Essential Commands"):
             f":satellite_orbital: `{round(self.bot.latency * 1000, 1)}ms`"
         )
 
+    @commands.command(pass_context=True)
+    async def help(self, ctx):
+        """
+        Get command information.
+
+        {help [optional: command name]}
+        """
+        embed = discord.Embed()
+        for el in self.bot.cogs:
+            sub = self.bot.get_cog(el)
+            formatted = ""
+            for x in sub.get_commands():
+                formatted += f"{x.name}, "
+            if formatted == "":
+                pass
+            else:
+                embed.add_field(
+                    name=sub.qualified_name, value=formatted[:-2], inline=False
+                )
+        await ctx.send(embed=embed)
+
     @commands.command()
     async def about(self, ctx):
         """
@@ -27,6 +49,7 @@ class Essentials(commands.Cog, name="Essential Commands"):
         embed = discord.Embed(title="Nanocord - About")
         embed.description = "Created by [@jacc](https://github.com/jacc), heavily inspired by [@venoras/Huecord](https://github.com/venoras/Huecord). \n\nCreated with :heart_decoration: in the USA (and under 2 hours!)\n\nAll rights reserved to the respective owners of copyrighted material, I don't know how legal stuff works but I don't own or claim to own the name Nanoleaf Auroras or Nanoleaf at all. If that's a problem contact me?"
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Essentials(bot))
