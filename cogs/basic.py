0e8d0362f8e8b738274f479fd072b7966d193a69
Jack LaFond <jack.lafond@icloud.com>
2020-06-20 23:56:19 -0400

    about command

--- cogs/basic.py
+++ cogs/basic.py
@@ -18,6 +18,15 @@ class Essentials(commands.Cog, name="Essential Commands"):
         m = await ctx.send(
             f":satellite_orbital: `{round(self.bot.latency * 1000, 1)}ms`"
         )
-        
+
+    @commands.command()
+    async def about(self, ctx):
+        """
+        Learn about the project!
+        """
+        embed = discord.Embed(title="Nanocord - About")
+        embed.description = "Created by [@jacc](https://github.com/jacc), heavily inspired by [@venoras/Huecord](https://github.com/venoras/Huecord). \n\nCreated with :heart_decoration: in the USA (and under 2 hours!)\n\nAll rights reserved to the respective owners of copyrighted material, I don't know how legal stuff works but I don't own or claim to own the name Nanoleaf Auroras or Nanoleaf at all. If that's a problem contact me?"
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Essentials(bot))
